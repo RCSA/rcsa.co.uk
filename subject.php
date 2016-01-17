12703fff006da91283024036a6993d0b62e2ff07
dalcde <dalcde@yahoo.com.hk>
2016-01-17 12:04:38 +0000

    JSONify subject reps

--- subject.php
+++ subject.php
@@ -3,42 +3,28 @@ require "header.php";
 ?>
 
 <p>If you are considering making an application to Robinson, and would like to know more about studying your chosen subject at Robinson, then feel free to contact one of the student subject reps listed below. All addresses are @cam.ac.uk.</p>
+
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>Subject</th>
+      <th>Society</th>
       <th>Name</th>
       <th>Email</th>
     </tr>
   </thead>
   <tbody>
-    <tr><td>Asian and Middle Eastern Studies</td><td>Anna Spaniol</td><td>ans57</td></tr>
-    <tr><td></td><td>Gary He</td><td>jhh46</td></tr>
-    <tr><td>Classics</td><td>Hugh Granger</td><td>hgwg2</td></tr>
-    <tr><td>Computer Science</td><td>Devan Kuleindiren</td><td>dk503</td></tr>
-    <tr><td></td><td>Alex Dalgleish</td><td>amd96</td></tr>
-    <tr><td>Economics</td><td>Karl Thompson</td><td>kt426</td></tr>
-    <tr><td>Engineering</td><td>Sarah Wolman</td><td>sw690</td></tr>
-    <tr><td>English</td><td>Maud Foxley</td><td>mf554</td></tr>
-    <tr><td></td><td>Jake Morris</td><td>jsm81</td></tr>
-    <tr><td>Geography</td><td>Holly Carter</td><td>hsc31</td></tr>
-    <tr><td>History</td><td>Harry Normanton</td><td>hn288</td></tr>
-    <tr><td></td><td>Emily Fishman</td><td>ejf51</td></tr>
-    <tr><td>Human, Social and Political Sciences</td><td>Martha Krish</td><td>mk814</td></tr>
-    <tr><td></td><td>Michael Palmer</td><td>mep45</td></tr>
-    <tr><td>Land Economy</td><td>Hoang Van Bui</td><td>hvb27</td></tr>
-    <tr><td>Law</td><td>Carine Ha</td><td>ch677</td></tr>
-    <tr><td>Mathematics</td><td>Laurence Cooper</td><td>ljc60</td></tr>
-    <tr><td>Medicine</td><td>Cammy Mitchell</td><td>lcm55</td></tr>
-    <tr><td></td><td>Tiffany Fung</td><td>thwf2</td></tr>
-    <tr><td>Music</td><td>Zhuan Faraj</td><td>zf234</td></tr>
-    <tr><td>Natrual Sciences (Biological)</td><td>Robyn Hockley</td><td>rh606</td></tr>
-    <tr><td></td><td>Patrick Lundgren</td><td>prl40</td></tr>
-    <tr><td>Natrual Sciences (Physical)</td><td>Will Scott</td><td>wjps2</td></tr>
-    <tr><td>Psychological and Behavioural Sciences</td><td>Charlotte Diss</td><td>cd542</td></tr>
-    <tr><td>Theology and Religious Studies</td><td>Beth Soman</td><td>bs547</td></tr>
-    <tr><td></td><td>Henry Constable</td><td>hc433</td></tr>
-    <tr><td>Veterinary Medicine</td><td>Emily Atkinson</td><td>ea406</td></tr>
+<?php
+$json = json_decode(file_get_contents("data/subjects.json"), true);
+if ($json) {
+    $first = key($json);
+    foreach ($json as $subject => $contacts) {
+      foreach ($contacts as $crsid => $name) {
+        echo "<tr><td>$subject</td><td>$name</td><td>$crsid</td>";
+        $subject = "";
+      }
+    }
+}
+?>
   </tbody>
 </table>
 <?php require "footer.php"?>
