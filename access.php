41b20ebe07be54544712f93f03daeb5ffcf564ca
dalcde <dalcde@yahoo.com.hk>
2016-03-01 08:08:02 +0000

    Add some content

--- access.php
+++ access.php
@@ -1,6 +1,8 @@
 <?php
 require "header.php";
 ?>
-<p>The Students of Robinson <a href="https://www.facebook.com/Students-of-Robinson-949754768431012/"><span class="glyphicon glyphicon-link"></span>Facebook</a> and <a href="http://instagram.com/studentsofrobinson"><span class="glyphicon glyphicon-link"></span>Instagram</a> pages shares how current students think about Robinson.
+<p>The Students of Robinson <a href="https://www.facebook.com/Students-of-Robinson-949754768431012/"><span class="glyphicon glyphicon-link"></span>Facebook</a> and <a href="http://instagram.com/studentsofrobinson"><span class="glyphicon glyphicon-link"></span>Instagram</a> pages shares how current students think about Robinson.</p>
+
+<p>We also have a student access <a href="http://robinsonslo.tumblr.com/"><span class="glyphicon glyphicon-link"></span>blog</a>.</p>
 
 <?php require "footer.php"?>
--- general-welfare.php
+++ general-welfare.php
@@ -86,6 +86,7 @@ if ($json) {
   <li>The Disabled Students' Campaign: <a href="https://www.facebook.com/groups/363775395562/?fref=ts">https://www.facebook.com/groups/363775395562/?fref=ts</a></li>
   <li>Cambridge Ask-A-Student: <a href="https://www.facebook.com/groups/200749143308432/">https://www.facebook.com/groups/200749143308432/</a></li>
   <li>Fly (BME Women's Network, for self-identifying women of Colour only) <a href="https://www.facebook.com/groups/359049374186552/?fref=nf">https://www.facebook.com/groups/359049374186552/?fref=nf</a></li>
+  <li>Soar (BME Men's Network) <a href="https://www.facebook.com/groups/ThisIsSoar/?fref=ts">https://www.facebook.com/groups/ThisIsSoar/?fref=ts</a></li>
 </ul>
 <h3>Other contacts</h3>
 <ul>
