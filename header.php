3950ab56286b130f6da0ad710fcd9d5348ee60ee
dalcde <dalcde@yahoo.com.hk>
2016-03-11 14:56:57 +0000

    Don't use external assets

--- header.php
+++ header.php
@@ -30,11 +30,11 @@ if ($result) {
 <html>
   <head>
     <title><?php echo $title?> | Robinson College Students' Association</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/files/lib/bootstrap.min.css">
     <link rel="stylesheet" href="/files/lib/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="/files/lib/jquery.min.js"></script>
+    <script src="/files/lib/bootstrap.min.js"></script>
   </head>
 
   <body>
