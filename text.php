ec3814f683fa68ebe5792e38eabcda808bf510bd
dalcde <dalcde@yahoo.com.hk>
2016-09-21 01:22:08 +0100

    Added a few access pages

---
+++ text.php
@@ -0,0 +1,52 @@
+<?php
+require "header.php";
+?>
+<h1>Texts from students</h1>
+<blockquote>
+<p>Robinson has loads active societies, sports (rowing, hockey, football, rugby, cricket, water polo and many more), music, film, theatre and many more. I loved my time here so far and honestly would not want to be anywhere else</p>
+
+<footer>Cammy Mitchell (second year Medicine)</footer>
+</blockquote>
+
+<blockquote>
+<p>At Robinson I’ve found the perfect balance between the “real world” and the sort of Cambridge dream that I envisaged prior to arriving here. Not only are you receiving a first-class education but you’re also in a space where it’s really easy to remove yourself from the grandeur of Cambridge when needs be. I feel really lucky to be surrounded by so many brilliant people. Instead of feeling intimidated, I’m motivated to improve alongside the great minds around me and use that knowledge in a really applicable way. I’ve developed the confidence to really express myself. There’s an atmosphere of normality here that strikes the perfect balance; a space that you can thrive in regardless of social background. I think that’s really rare no matter what university you’re at. Choosing Robinson is one of those decisions I’ll thank myself for, now and in years to come.</p>
+<footer>Elle Shea (second year MML)</footer>
+</blockquote>
+
+<blockquote>
+<p>Robinson (aka the Red Fortress), it’s definitely the best place to have a balanced and fun life in Cambridge. The College also has great accommodation and gardesn and a nice and relaxed atmosphere.</p>
+<footer>Van (second year land economist)</footer>
+</blockquote>
+
+<blockquote>
+<p>Robinson is friendly and relaxed, a great place to study and socialise.</p>
+<footer>Harry Normanton (second year historian)</footer>
+</blockquote>
+
+<blockquote>
+<p>What I love at Robinson is waking up in the morning and seeing our deer &mdash; wildlife and nature is at the heart of our college environment.</p>
+<footer>Anna Spaniol (second year AMES)</footer>
+</blockquote>
+
+<img class="float-right-responsive" src="/photos/talk.jpg"></src>
+
+<blockquote>
+<p>Its sad my time has come to an end at Robinson, I have learnt so many new skills and experiences which I will take with me.</p>
+<footer>Ben peacock (PCGE student)</footer>
+</blockquote>
+
+<blockquote>
+<p>Robinson has given me so many opportunities to improve my academic, social and personal skills and also learn new ones, I feel more confident and ready to apply myself for future graduate schemes and am excited for whatever the future brings.</p>
+<footer>Rohan (third year Philosophy)</footer>
+</blockquote>
+
+<blockquote>
+<p>Robinson College truly feels like a second home, when I get home I miss the red bricks and can’t wait to come back. The café and gardens are my favourite places – the gardens are well maintained and lovely to spent time in when its summer. The Red Brick café is also a favourite hangout, the staff here are so friendly and the café it is a really convenient place to work and socialise – something which a lot of other colleges don’t have.</p>
+<footer>Emily Fishman (second year historian)</footer>
+</blockquote>
+
+<blockquote>
+<p>The vibrant community atmosphere and excellent social and academic facilities make Robinson a great college to both live and study in.</p>
+<footer>Luke Dell (first year Geography)</footer>
+</blockquote>
+<?php require "footer.php"?>
---
+++ why-robinson.php
@@ -0,0 +1,43 @@
+<?php
+require "header.php";
+?>
+<section>
+<h1>Why Robinson</h1>
+<p>So why choose Robinson? Picking a Cambridge college can feel like a mind boggling task, especially with every college trying to convince you that they are the best for you. This section aims to explore what Robinson College has to offer and what features make Robinson College unique. Robinson may well be the college for you.</p>
+<img style='width:100%' src="/photos/robinson_front.jpg"> </img>
+</section>
+
+<section>
+  <h2>The Alternative choice</h2>
+<p>  Robinson College is the newest college in Cambridge, built entirely out of red bricks it is the quirkiest, coolest college out there. For those of you looking for an alternative to the traditional Cambridge colleges, then Robinson College is the place for you.</p>
+</section>
+
+<section>
+  <h2>Friendly and welcoming</h2>
+<p>Robinson strives on being a friendly college, we have a diverse range of students from all over the world and from many different backgrounds studying at Robinson. The college atmosphere is fun, laid back and friendly. All first years live in college accommodation, with second and third years having the freedom of choice to live in a house on Adams road or Sylvester road, or live in college. Living in college means you develop a close network of friends with each staircase having its own unique community, housing students from all over the world.</p>
+</section>
+
+<section>
+  <h2>The Gardens</h2>
+<img class='float-right-responsive' src="/photos/garden.jpg"></img>
+<p>The gardens are beautiful and tranquil a perfect place to relax after a day studying in the library, we even have a resident deer. With beautiful gardens hidden behind the red brick castle and maze of the college houses, the environment is warm and welcoming. Unlike other colleges these gardens are open to all members of the college and strolling on the grass to enjoy them is completely allowed! Students enjoy the gardens the most in summer eating their lunches on the lawn outside, playing croquet and soaking up the sun being common sight. Our college gardens as well as being pretty and a nice place to chill out in, are historically special being home to some interesting species such as the “dementor tree” by Thorney creek.  If you’re really lucky you might also spot the deer that’s also made this little oasis her home, otherwise it’s just the birds, cats (and students) that contribute to the local wildlife.</p>
+</section>
+
+<section>
+  <h2>Food and Red brick Café</h2>
+<img class='float-left-responsive' src="/photos/cafe.jpg"></img>
+<p>Robinson has a café and a restaurant – unlike other colleges we have the best of both worlds. The café is open during the day and transforms into a bar during the evening, The café has a good range of sandwiches, wraps and sushi (I would recommend the panini’s) and an excellent selection of hot drinks, smoothies, milkshakes and diverse tea selection (everything from earl grey to green). They also stock emergency supplies – eggs, bread and milk. The Garden Restaurant opens out onto the Robinson gardens and opens – Mon-Fri (Breakfast 8am-9am, Lunch 12:30-2, and dinner from 6:7:15pm), at the weekends it has brunch from 12 (which is the highlight of every student’s weekend). The garden restaurant (or GR) has excellent vegan and vegetarian options too, and the staff are more than happy to help cater for special dietary requirements. We also serve formal hall (Tuesday and Friday) which requires a gown, and involves a three course set menu with lots of fancy cutlery. Formals are a very special occasion, they are NOT compulsory, but are a lovely event to attend.  Despite Robinson being a modern college, we keep with tradition and formal hall is an example of this.</p>
+</section>
+
+<section>
+  <h2>Library and study resources</h2>
+<img class='float-right-responsive' src="/photos/lib.jpg"></img>
+ <p>We also have a well-stocked library, a great place for productive work. The Robinson library holds over 50,000 books, all in good order. The layout is easy to follow, with the science book (medicine, chemistry, veterinary, and physics) history of art and DVD collection being upstairs. English literature, German, French, Russian and Italian (as well as many other language books) being situated on the ground floor. Classics, history, geography, philosophy, theology and social sciences being located on the lower floor. The library also stocks a printer room, a photocopy room and offers a great range of desks with plug points for laptops. The library as a rule of no food or drink, but water in a bottle is allowed. The library is also open 24hours, but office hours from 9-5pm weekdays. The librarian is on hand at these times if you need help or assistance. To take books out requires your university card, and a simply scanning process which is signposted. To enter the library your university card is also needed. It’s important to respect the space of the library, bins are placed for rubbish and in the evenings noise is kept to a minimum to ensure a good working environment.</p>
+</section>
+
+<section>
+  <h2>Location</h2>
+<p>Robinson has a perfect location, with the college being located just outside the busy centre of Cambridge (and opposite the University Library woo..) you can avoid the hustle and bustle of the city, and tourists whilst still being close enough to walk there and back in 20 minutes.</p>
+</section>
+</ul>
+<?php require "footer.php"?>
